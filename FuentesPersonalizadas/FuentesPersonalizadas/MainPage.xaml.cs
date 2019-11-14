feea4ac998e3a138190330cc53420f3c9d8a4a77
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2019-11-13 22:45:14 -0600

    Carga del demo generado hoy

---
+++ FuentesPersonalizadas/FuentesPersonalizadas/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FuentesPersonalizadas
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // cargando el icono desde codigo
+
+            imgIcono2.Source = new FontImageSource
+            {
+                FontFamily = (OnPlatform<string>)Application.Current.Resources["MaterialFontFamily"],
+                Size = 30,
+                Color = Color.Brown,
+                Glyph = IconFont.GoogleAllo
+            };
+        }
+    }
+}
